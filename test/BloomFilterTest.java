16aaf008d6308a4467bef81fef3f9cbda1383928
Nikesh White <Nikesh.Social@yandex.ru>
2018-12-23 21:34:50 +0300

    Added tests for method size

--- test/BloomFilterTest.java
+++ test/BloomFilterTest.java
@@ -5,12 +5,31 @@ public class BloomFilterTest {
 
         @Test
         public void contains() {
-            BloomFilter<Integer> bloomFilter = new BloomFilter<>(127, 3);
+            BloomFilter<Integer> bloomFilter = new BloomFilter<>(128, 3);
 
             assertFalse(bloomFilter.contains(0));
             assertFalse(bloomFilter.contains(-99999999));
             assertFalse(bloomFilter.contains(1000000000));
         }
+
+    @Test
+    public void size() {
+        BloomFilter<Integer> bloomFilter = new BloomFilter<>(128, 3);
+
+        bloomFilter.add(1);
+        bloomFilter.add(5);
+        bloomFilter.add(4);
+        bloomFilter.add(3);
+        bloomFilter.add(2);
+        bloomFilter.add(11);
+        bloomFilter.add(12);
+
+        assertEquals(7, bloomFilter.size());
+
+        BloomFilter<Integer> bloomFilter2 = new BloomFilter<>(32, 3);
+
+        assertEquals(0, bloomFilter2.size());
+    }
 
 
 }
