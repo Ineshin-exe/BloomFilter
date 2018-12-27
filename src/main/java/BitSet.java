b7b7b53951b5f65da43c98c3c5371b04fd124db8
Nikesh White <Nikesh.Social@yandex.ru>
2018-12-28 01:47:53 +0300

    Added checks

--- src/main/java/BitSet.java
+++ src/main/java/BitSet.java
@@ -12,7 +12,12 @@ public class BitSet {
      */
 
     BitSet(int size) {
-        this.set = new int[size / BIT_SET + 1];
+        if (size < 0) throw new IllegalArgumentException("Size of BitSet can not be negative");
+        else if (size == 0) this.set = new int[0];
+        else {
+            if (size % BIT_SET == 0) this.set = new int[size / BIT_SET];
+            else this.set = new int[size / BIT_SET + 1];
+        }
         this.size = size;
     }
     /**
@@ -21,7 +26,8 @@ public class BitSet {
      * @throws IllegalArgumentException sets a number larger than the BitSet size.
      */
     void add (int num) {
-        if (num <= size && num >= 0) {
+        if (size == 0) throw new IllegalArgumentException("Size of BitSet == 0");
+        else if (num <= size && num >= 0) {
             int a = num / BIT_SET;
             int b = num % BIT_SET;
             if ((set[a] & (1 << b)) == 0) {
@@ -38,7 +44,8 @@ public class BitSet {
      * @throws IllegalArgumentException sets a number larger than the BitSet size.
      */
     boolean contains (int num) {
-        if (num <= size && num >= 0) {
+        if (size == 0) return false;
+        else if (num <= size && num >= 0) {
             int a = set[num / BIT_SET];
             int b = num % BIT_SET;
             return (a & (1 << b)) != 0;
